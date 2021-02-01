2ae41fc3373d4cdf24ad97fc28be828cdc47697b
Azertinv <azertinv@protonmail.com>
2021-02-01 22:17:40 +0100

    +answer checking on Question refactor

--- report/Question.gd
+++ report/Question.gd
@@ -3,23 +3,40 @@ extends HBoxContainer
 export(String) var question_id = ""
 
 var answer: String
+var answer_format: String
 
 func _ready():
 	if question_id == "":
 		push_error("NewQuestion spawned with no id")
 		return
 	var data = ReportLoader.questions[question_id]
-	answer = data["answer"]
-	Helper.init_format(self, data["format"], data["possibilities"])
+	answer_format = data["answer"]
+	answer = answer_format.replace("%a", "")
+	answer = answer.replace("%t", "")
+	Helper.init_format($Answer, data["format"], data["possibilities"])
 
-#TODO
-func is_good_answer() -> bool:
-	return false
+func get_answer_helper(answer_box) -> String:
+	var result = ""
+	for c in answer_box.get_children():
+		if c is Label:
+			result += c.text + " "
+		elif c is LineEdit:
+			result += c.get_number() + " "
+		elif c is CanvasLayer:
+			pass
+		else: # go down one layer
+			result += get_answer_helper(c)
+	return result
 
 func get_answer() -> String:
-	return ""
+	return get_answer_helper($Answer).strip_edges()
 
-func mark_question_as_answered():
-	pass
+func is_good_answer() -> bool:
+	return answer == get_answer()
+
+func mark_question_as_answered() -> void:
+	$CheckBox.pressed = true
+	$Answer.queue_free()
+	Helper.init_format(self, answer_format, [])
 
 #TODO save system
